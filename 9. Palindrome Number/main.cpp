c69fb9c5d427779274b799a4a7d055520a92f8f2
lingme <kid--l@hotmail.com>
2020-04-14 20:15:04 +0800

    add 9. Palindrome Number mode 2

--- 9. Palindrome Number/main.cpp
+++ 9. Palindrome Number/main.cpp
@@ -5,16 +5,32 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
+
+    }
+
+    bool isPalindrome1(int x) {
         auto x_string = to_string(x);
+        int i = 0;
+        while (i < x_string.size() / 2) {
+            if (x_string[i] != x_string[x_string.size() - i - 1])
+                break;
+            i++;
+        }
+        return i == x_string.size() / 2 ? true : false;
+    }
+
+    bool isPalindrome2(int x) {
+        string s = to_string(x);
+        return s == string(rbegin(s), rend(s));
     }
 };
 
 int main() {
     auto solution = new Solution();
     cout << solution->isPalindrome(100) << endl;
-    cout << solution->isPalindrome(101) << endl;
+    cout << solution->isPalindrome(-101) << endl;
     cout << solution->isPalindrome(1034) << endl;
     cout << solution->isPalindrome(1003001) << endl;
-    cout << solution->isPalindrome(100003030001) << endl;
+    cout << solution->isPalindrome(100303001) << endl;
     return 0;
 }
